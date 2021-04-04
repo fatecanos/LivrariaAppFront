4ab35c489d4853dc21e39c68a6682d236b8a679f
Lucas Nogueira <lucasnogueiratdm@hotmail.com>
2021-04-04 20:27:03 -0300

    adicionando novo endpoint, service de estoque

--- src/app/services/estoque-service/estoque.service.ts
+++ src/app/services/estoque-service/estoque.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LivroEstoqueInterface } from 'src/app/models/interfaces/dto/estoque.interface';
+import { ItemEstoqueDTO, LivroEstoqueInterface } from 'src/app/models/interfaces/dto/estoque.interface';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -18,5 +18,9 @@ export class EstoqueService {
 
   obterDetalhesLivroPorId(id: number): Observable<LivroEstoqueInterface> {
     return this.http.get<LivroEstoqueInterface>(`${this.baseUrl}/estoque/${id}`)
+  }
+
+  obterLivrosGestaoEstoque(): Observable<ItemEstoqueDTO[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/gestao-estoque`);
   }
 }
