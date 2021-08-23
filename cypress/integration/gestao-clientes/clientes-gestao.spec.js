5486beacd9f4da9cb47cdff2348f278bdb16a397
Lucas Nogueira <lucasnogueiratdm@hotmail.com>
2021-08-23 01:58:13 -0300

    testes: adiocionando configs de testes

---
+++ cypress/integration/gestao-clientes/clientes-gestao.spec.js
@@ -0,0 +1,80 @@
+describe('Gestão de Clientes - Modulo', function() {
+    
+    it('RF0021 - Deve cadastrar um cliente', function() {
+        this.skip()
+        cy.visit('http://localhost:4200/livraria/novo-cliente'); 
+        cy.get('.client-form-input').within(()=> {
+            cy.get('#nome')
+                .type('Lucas')
+                
+            cy.get('#sobrenome')
+                .type('Nogueira')
+
+            cy.get('#dataNascimento')
+                .type('1998-12-12')
+            
+            cy.get('#cpf')
+                .type('47126964862')
+
+            cy.get('#rdMasculino')
+                .click()
+
+            cy.get('#email')
+                .type('lucasnogueira@gmail.com')
+
+            cy.get('#telefone')
+                .type('11997477173')
+            
+            cy.get('#senha').type('lucas123')
+            cy.get('#repeteSenha').type('lucas123')
+
+            cy.get('.client-form-button').within(()=> {
+                cy.get('#btnEnviar').click()
+            })
+        });        
+    }),
+
+    it('RF0022 - Deve alterar dados cadastrais do cliente', ()=> {
+        cy.visit('http://localhost:4200/livraria/login'); 
+
+        cy.get('.painel_login-container').within(()=> {
+            cy.get('#inputEmail')
+                .type('lucas@gmail.com')
+            cy.get('#inputSenha')
+                .type('12345')
+        });
+
+        cy.get('.painel_login-container-form > button').click();
+        cy.visit('http://localhost:4200/clientes')
+
+        //Dados cliente
+        cy.get('#txtNome').clear().type('Gabriel')
+        cy.get('#sobrenome').clear().type('da Luz')
+        cy.get('#dataNascimento').clear().type('1998-04-06')
+        cy.get('#cpf').clear().type('47126964862')
+        cy.get('#btnAtualizaDados').click()
+
+        // //Atualiza Telefone
+        // cy.get('div[role=tab]').eq(1).click()
+
+        // //Endereco
+        // cy.get('div[role=tab]').eq(2).click()
+
+        //Email
+        cy.get('div[role=tab]').eq(3).click()
+        cy.get('#txtEmail').type('gabriel@gmail.com')
+        cy.get('#txtConfirmaEmail').type('gabriel@gmail.com')
+        cy.get('#btnEmail').click()
+        cy.get('div[role=tab]').eq(3).click()
+
+        // Senha
+        cy.get('div[role=tab]').eq(4).click()
+        cy.get('#txtSenha').type('gabriel')
+        cy.get('#txtConfirmaSenha').type('gabriel')
+        cy.get('#btnAtualizaSenha').click()
+        cy.get('div[role=tab]').eq(4).click()
+        // //Credito
+        // cy.get('div[role=tab]').eq(5).click().click()
+
+    })
+})
--- src/app/components/dialogs/inativar-cartao-dialog/inativar-cartao-dialog.component.ts
+++ src/app/components/dialogs/inativar-cartao-dialog/inativar-cartao-dialog.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CartaoDTO } from 'src/app/models/interfaces/dto/cartao.interface';
+import { CartaoClienteDTO } from 'src/app/models/interfaces/dto/client.interface';
+import { CartoesService } from 'src/app/services/cartoes-service/cartoes.service';
 
 @Component({
   selector: 'inativar-cartao-dialog',
@@ -10,13 +14,25 @@ export class InativarCartaoDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<any>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: CartaoClienteDTO,
+    private service: CartoesService,
+    private snackService: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
 
   }
   
   inativarCartao() {
-    console.log("inativando cartao:", this.data.cartao);
+    console.log('dados modal:',this.data.id);
+    
+    this.service.removerCartao(this.data.id).subscribe(res => {
+      this.snackService.open('cartão foi removido com sucesso', 'fechar', {duration: 3000})
+    }, err => {
+      this.snackService.open('falha ao remover cartão', 'fechar', {duration: 3000})
+    }, 
+    ()=> {
+      this.dialogRef.close();
+    })
   }
 }
