49038a829aebe5a3e219550d98b8ccecdcdaf0a6
Lucas Nogueira <lucasnogueiratdm@hotmail.com>
2021-09-25 17:20:41 -0300

    adicionando form novo cartao

--- src/app/pages/livraria-domain/formulario-cartoes/formulario-cartoes.component.ts
+++ src/app/pages/livraria-domain/formulario-cartoes/formulario-cartoes.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { CartaoCreditoDTO } from 'src/app/models/interfaces/dto/cartao.interface';
-import { CartaoClienteDTO } from 'src/app/models/interfaces/dto/client.interface';
+import { BandeiraCartaoClienteDTO, CartaoClienteDTO } from 'src/app/models/interfaces/dto/client.interface';
+import { bandeirasMock } from 'src/app/models/mocks/bandeiras-cartao.mock';
 import { CartoesService } from 'src/app/services/cartoes-service/cartoes.service';
 
 @Component({
@@ -15,6 +16,7 @@ export class FormularioCartoesComponent implements OnInit {
   form: FormGroup = new FormGroup({})
 
   cartoesCliente$?: Observable<CartaoCreditoDTO[]>;
+  bandeiras: Array<BandeiraCartaoClienteDTO> = bandeirasMock;
 
   constructor(
     private cartaoService: CartoesService,
@@ -32,6 +34,7 @@ export class FormularioCartoesComponent implements OnInit {
             bandeira: this.formBuilder.control('', Validators.required),
             codigoSeguranca: this.formBuilder.control('', Validators.required),
             isNovoCartao: this.formBuilder.control(false, Validators.required),
+            isGravarNovo: this.formBuilder.control(false, Validators.required),
             valorPago: this.formBuilder.control(0, Validators.required)
           })
         ])
@@ -68,6 +71,11 @@ export class FormularioCartoesComponent implements OnInit {
       bandeira: cartao.bandeira,
       codigoSeguranca: cartao.codigoSeguranca,
     })
+  }
+
+  hidenNumbers(numeroCartao: string) {
+    const rgx = /\d\d\d\d.\d\d\d\d.\d\d\d\d./;
+    return numeroCartao.replace(rgx, numeroCartao);
   }
 
 }
