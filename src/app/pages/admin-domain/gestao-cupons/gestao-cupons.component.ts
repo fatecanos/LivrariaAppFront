4e1c3b17c693f0df9a468b40852c0c2ca5f1762c
G-Luz <g.luzsilva1@gmail.com>
2021-11-24 19:02:42 -0300

    feat: register and list cupons promocionais

--- src/app/pages/admin-domain/gestao-cupons/gestao-cupons.component.ts
+++ src/app/pages/admin-domain/gestao-cupons/gestao-cupons.component.ts
@@ -1,33 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import { CupomDTO } from 'src/app/models/interfaces/dto/cupom.interface';
+import { CupomService } from 'src/app/services/cupons-service/cupom.service';
 
-const ELEMENT_DATA: any[] = [
-  {id: 1, codigo: '2334', nome: 'Ofertas Março', valor: 30.00 },
-  {id: 2, codigo: '2333', nome: 'Ofertas dia dos Pais', valor: 50.00 },
-  {id: 3, codigo: '2389', nome: 'Ofertas Familia', valor: 9.00 },
-  {id: 4, codigo: '2309', nome: 'Ofertas Bons Livros', valor: 5.00 }
-];
+// const ELEMENT_DATA: any[] = [
+//   { id: 1, codigo: '2334', nome: 'Ofertas Março', valor: 30.0 },
+//   { id: 2, codigo: '2333', nome: 'Ofertas dia dos Pais', valor: 50.0 },
+//   { id: 3, codigo: '2389', nome: 'Ofertas Familia', valor: 9.0 },
+//   { id: 4, codigo: '2309', nome: 'Ofertas Bons Livros', valor: 5.0 },
+// ];
 
 @Component({
   selector: 'liv-gestao-cupons',
   templateUrl: './gestao-cupons.component.html',
-  styleUrls: ['./gestao-cupons.component.scss']
+  styleUrls: ['./gestao-cupons.component.scss'],
 })
 export class GestaoCuponsComponent implements OnInit {
-
   isNovoCupomForm: boolean = false;
   cupomForm: FormGroup = new FormGroup({});
 
   displayedColumns: string[] = ['id', 'codigo', 'nome', 'valor', 'opcoes'];
-  dataSource = ELEMENT_DATA;
+  dataSource: CupomDTO[] = [];
 
-  constructor(private formBuilder: FormBuilder) { }
+  cupons$?: Observable<CupomDTO[]>;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CupomService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
+    this.cupons$ = this.service.obterTodosCuponsPromocionais();
+
+    this.cupons$.subscribe((response) => {
+      this.dataSource = response;
+    });
     this.cupomForm = this.formBuilder.group({
       nome: ['', Validators.required],
-      valor: [0, Validators.required]
-    })
+      valor: [0, Validators.required],
+      tipoCupom: 0,
+    });
   }
 
+  salvarNovoCupom() {
+    let payload = this.cupomForm?.value;
+    console.log(payload);
+
+    this.service.save(payload).subscribe(
+      (res) => {
+        this._snackBar.open(`Novo cupom cadastrado com sucesso`, 'fechar');
+      },
+      (err) => {
+        this._snackBar.open(err.error.description, 'fechar');
+      },
+      () => {
+        this.ngOnInit();
+      }
+    );
+  }
 }
